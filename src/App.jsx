048050a24ebc3359aa6efd3f4675609832f33235
treadhall8-glitch <treadhall8@gmail.com>
2026-07-07 13:14:50 +0530

    Fix App routing

--- src/App.jsx
+++ src/App.jsx
@@ -6,45 +6,58 @@ import Register from "./pages/Register";
 import AdminDashboard from "./pages/AdminDashboard";
 import SellerDashboard from "./pages/SellerDashboard";
 import Checkout from "./pages/Checkout";
+import Cart from "./pages/Cart";
+
 import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
     <BrowserRouter>
-      <Route
-  path="/admin"
-  element={
-    <ProtectedRoute role="admin">
-      <AdminDashboard />
-    </ProtectedRoute>
-  }
-/>
+      <Routes>
 
-<Route
-  path="/seller"
-  element={
-    <ProtectedRoute role="seller">
-      <SellerDashboard />
-    </ProtectedRoute>
-  }
-/>
+        <Route path="/" element={<Home />} />
 
-<Route
-  path="/checkout"
-  element={
-    <ProtectedRoute>
-      <Checkout />
-    </ProtectedRoute>
-  }
-/>
+        <Route path="/login" element={<Login />} />
 
-<Route
-  path="/cart"
-  element={
-    <ProtectedRoute>
-      <Cart />
-    </ProtectedRoute>
-  }
-/>
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/cart"
+          element={
+            <ProtectedRoute>
+              <Cart />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/checkout"
+          element={
+            <ProtectedRoute>
+              <Checkout />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/seller"
+          element={
+            <ProtectedRoute role="seller">
+              <SellerDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute role="admin">
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+      </Routes>
     </BrowserRouter>
   );
 }
